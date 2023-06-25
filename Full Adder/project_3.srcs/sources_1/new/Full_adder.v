d3558e3c2a9bc4b83b2693f07e2b41e9a302a0f1
jatinlohar <95520913+jatinlohar@users.noreply.github.com>
2023-06-25 19:50:55 +0530

    Add files via upload

---
+++ Full Adder/project_3.srcs/sources_1/new/Full_adder.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 17.01.2023 14:20:53
+// Design Name: 
+// Module Name: Full_adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Full_adder(
+
+    );
+endmodule
